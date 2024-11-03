c1049f7ac64414e4798d249d43642ee821b40c1c
cmd-anurag <153152868+cmd-anurag@users.noreply.github.com>
2024-11-03 20:42:30 +0530

    fixed game over

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,8 +1,19 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameStateManager : MonoBehaviour
 {
+
+    // EVENT SUBSCRIPTIONS
+    private void OnEnable() {
+        SpaceshipControllerScript.OnSpaceShipDeath += LoadGameOverScene;
+    }
+
+    private void OnDisable() {
+        SpaceshipControllerScript.OnSpaceShipDeath -= LoadGameOverScene;
+    }
+
     public void ReplayGame() {
         SceneManager.LoadScene("GameScene");
     }
@@ -12,4 +23,14 @@ public class GameStateManager : MonoBehaviour
     public void QuitGame() {
         Application.Quit();
     }
+
+    public void LoadGameOverScene(Transform pos) {
+        StartCoroutine(LoadGameOverAfterDelay(2f));
+    }
+
+    private IEnumerator LoadGameOverAfterDelay(float delay=2f) {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("GameOver");
+    }
+
 }
